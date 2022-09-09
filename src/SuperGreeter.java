745f7be815bc4d7e1d62e2b20b07453b32602aba
heva0011 <heva0011@stud.kea.dk>
2022-09-09 09:55:02 +0200

    second commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,12 @@
+public class SuperGreeter {
+
+    public static void main(String[] args) {
+        SuperGreeter sg = new SuperGreeter();
+        sg.start();
+
+    }
+
+    public void start() {
+        System.out.println("Det virker!");
+    }
+}
